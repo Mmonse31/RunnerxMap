4abea5ac4f174c539ef4dd11fef148765e099add
Montserrat Montaño <0318119070@ut-tijuana.edu.mx>
2022-04-10 23:23:10 -0700

    create statistics page and dinamic bar chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,11 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },  {
+    path: 'statistics',
+    loadChildren: () => import('./statistics/statistics.module').then( m => m.StatisticsPageModule)
   },
+
 ];
 
 @NgModule({
---
+++ src/app/statistics/statistics.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+import { NgChartsModule } from 'ng2-charts';
+
+import { StatisticsPageRoutingModule } from './statistics-routing.module';
+
+import { StatisticsPage } from './statistics.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NgChartsModule,
+    StatisticsPageRoutingModule
+  ],
+  declarations: [StatisticsPage]
+})
+export class StatisticsPageModule {}
